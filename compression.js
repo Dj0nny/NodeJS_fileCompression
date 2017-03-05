fde7ed40c4d715c475382f6c4c6ca12731bf3230
Francesco Giambruno <frank96990@gmail.com>
2017-03-05 21:07:03 +0100

    Update compression.js

--- compression.js
+++ compression.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 var zlib = require('zlib');
 
-fs.createReadStream('input.txt')
+fs.createReadStream('input.txt') // creation of the reading stream with the file's name
 .pipe(zlib.createGzip()) // creation of a zip archive
 .pipe(fs.createWriteStream('input.txt.gz')); // creation of the write stream inside of the zip archive
 console.log("File Compressed.");
